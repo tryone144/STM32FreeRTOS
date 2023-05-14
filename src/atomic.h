0028700c2b78d54c5e7e3412fe1762b6c919ce37
Bernd Busse <bernd@busse-net.de>
2023-05-14 14:38:07 +0200

    Added new include wrapper

---
+++ src/atomic.h
@@ -0,0 +1,11 @@
+/*
+ * @file    atomic.h
+ * @author  Frederic Pillon <frederic.pillon@st.com> for STMicroelectronics.
+ * @brief   Include header file to match Arduino library format
+ */
+#ifndef _ARDUINO_ATOMIC_H_
+#define _ARDUINO_ATOMIC_H_
+
+#include "FreeRTOS/Source/include/atomic.h"
+
+#endif //_ARDUINO_ATOMIC_H_
